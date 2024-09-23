fd556f759d772eb7deb21f66bdd77def7a4dde43
Ryleeau <66577289+Ryleeau@users.noreply.github.com>
2024-09-22 15:13:44 -1000

    Done

---
+++ sumofnumbers.js
@@ -0,0 +1,32 @@
+"use strict";
+function sumFor(list) {
+    let sum = 0;
+    for (let i = 0; i < list.length; i++) {
+        sum += list[i];
+    }
+    return sum;
+}
+console.log("sumFor([1, 2, 3, 4]):", sumFor([1, 2, 3, 4])); // Output: 10
+function sumWhile(list) {
+    let sum = 0;
+    let i = 0;
+    while (i < list.length) {
+        sum += list[i];
+        i++;
+    }
+    return sum;
+}
+function sumRecursion(list) {
+    if (list.length === 0) {
+        return 0;
+    }
+    else {
+        return list[0] + sumRecursion(list.slice(1));
+    }
+}
+function sumTheFunctionalWay(list) {
+    return list.reduce((sum, current) => sum + current, 0);
+}
+console.log("sumWhile([1, 2, 3, 4]):", sumWhile([1, 2, 3, 4])); // Output: 10
+console.log("sumRecursion([1, 2, 3, 4]):", sumRecursion([1, 2, 3, 4])); // Output: 10
+console.log("sumTheFunctionalWay([1, 2, 3, 4]):", sumTheFunctionalWay([1, 2, 3, 4])); // Output: 10
---
+++ sumofnumbers.ts
@@ -0,0 +1,36 @@
+function sumFor(list: number[]):number {
+    let sum = 0;
+    for (let i = 0; i < list.length; i++) {
+        sum += list[i];
+    }
+    return sum;
+}
+console.log("sumFor([1, 2, 3, 4]):", sumFor([1, 2, 3, 4])); // Output: 10
+
+
+function sumWhile(list:number[]):number {
+    let sum = 0;
+    let i = 0;
+    while (i < list.length) {
+        sum += list[i];
+        i++;
+    }
+    return sum;
+}
+
+function sumRecursion(list:number[]):number {
+    if (list.length === 0) {
+        return 0;
+    } else {
+        return list[0] + sumRecursion(list.slice(1));
+    }
+}
+
+function sumTheFunctionalWay(list:number[]):number {
+    return list.reduce((sum:number, current:number) => sum + current, 0);
+}
+
+console.log("sumWhile([1, 2, 3, 4]):", sumWhile([1, 2, 3, 4])); // Output: 10
+console.log("sumRecursion([1, 2, 3, 4]):", sumRecursion([1, 2, 3, 4])); // Output: 10
+console.log("sumTheFunctionalWay([1, 2, 3, 4]):", sumTheFunctionalWay([1, 2, 3, 4])); // Output: 10
+
